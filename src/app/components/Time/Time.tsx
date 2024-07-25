159c75c8c990528f18669f99abae4e0925e06bd7
nurdin <nurdin@bdg.ebdesk.com>
2024-07-26 06:42:00 +0700

    add period

--- src/app/components/Time/Time.tsx
+++ src/app/components/Time/Time.tsx
@@ -11,12 +11,17 @@ const Time = (params: ITime) => {
 
   useEffect(() => {
     let timeDate: any
-    if (params?.date?.indexOf("&")) {
-      let splitted = params?.date.split("&");
-      timeDate = `${timeFormat(splitted[0], "MMM YYYY")} - ${timeFormat(splitted[1], "MMM YYYY")}`
-    } else
-      timeDate = timeFormat(params?.date, "MMM YYYY")
-    setShowTime(timeDate)
+    try {
+      if (params?.date?.indexOf("&") > -1) {
+        let splitted = params?.date.split("&");
+        timeDate = `${timeFormat(splitted[0], "MMM YYYY")} - ${timeFormat(splitted[1], "MMM YYYY")}`
+      } else
+        timeDate = timeFormat(params?.date, "MMM YYYY")
+      setShowTime(timeDate)
+    } catch (error) {
+
+    }
+
   }, [params.date])
 
   return (
